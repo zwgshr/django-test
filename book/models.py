664771a4d90e4b10729fb5275c590c48d5400519
zhangwg4apollo <zhangwenguang@apollosuite.com>
2025-01-07 18:33:42 +0800

    book rm

--- book/models.py
+++ book/models.py
@@ -1,6 +1,6 @@
 from django.db import models
 
-# Create your models here.
+
 class Book(models.Model):
     title = models.CharField(max_length=200)  # 书名
     author = models.CharField(max_length=100)  # 作者
